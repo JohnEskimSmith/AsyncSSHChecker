3e7accc08ecfccfd25afc687bf7323432b11c0c0
andrew.foma <andrew.foma@protonmail.com>
2023-10-09 12:09:00 +0300

    fix

--- ssh-fingerpint.py
+++ ssh-fingerpint.py
@@ -3,6 +3,8 @@ __author__ = "SAI"
 
 import argparse
 import asyncio
+import datetime
+
 
 from collections import namedtuple
 from time import time
@@ -28,6 +30,8 @@ CONST_STOP_STAGE = b"STOP"
 
 CONST_C = 1024
 CONST_DEFAULT_PORT = 22
+CONST_TIMEOUT = 7
+
 CONST_SERVER_HOST_KEY_ALGS = {"ssh-rsa": [md5, sha256],
                               "ecdsa-sha2-nistp256": [md5, sha256]}
 
@@ -89,7 +93,7 @@ async def write_to_stdout(object_file: BinaryIO,
     :param record_str:
     :return:
     """
-    await object_file.write(record_str.encode('utf-8') + b'\n')
+    await object_file.write(record_str.encode("utf-8") + b"\n")
 
 
 async def write_to_file(object_file: TextIO,
@@ -126,7 +130,7 @@ async def read_input_file(queue_input: asyncio.Queue,
                           settings: dict,
                           path_to_file: str) -> int:
     count_input = 0
-    async with aiofiles_open(path_to_file, mode='rt') as f:  # read str
+    async with aiofiles_open(path_to_file, mode="rt") as f:  # read str
         async for line in f:
             linein = line.strip()
             if any([check_ip(linein), check_network(linein)]):
@@ -150,22 +154,23 @@ async def work_with_queue_tasks(queue_results: asyncio.Queue,
 
 async def _get_server_host_key(target, *, tunnel=(), family=(), flags=0,
                                local_addr=None, client_version=(),
-                               kex_algs=(), config=(), options=None) -> dict | None:
+                               kex_algs=(), config=(), options=None) -> tuple[dict, str]:
 
     def conn_factory():
         current_loop = asyncio.get_event_loop()
-        return asyncssh.SSHClientConnection(current_loop, options, wait='kex')
+        return asyncssh.SSHClientConnection(current_loop, options, wait="kex")
 
     current_loop = asyncio.get_event_loop()
     host = target.ip
     port = target.port
     result = {}
+    error = ""
     for server_host_key_algs in CONST_SERVER_HOST_KEY_ALGS.keys():
         options = asyncssh.SSHClientConnectionOptions(
             options, config=config, host=host, port=port, tunnel=tunnel,
             family=family, local_addr=local_addr, known_hosts=None,
             server_host_key_algs=server_host_key_algs, x509_trusted_certs=None,
-            x509_trusted_cert_paths=None, x509_purposes='any', gss_host=None,
+            x509_trusted_cert_paths=None, x509_purposes="any", gss_host=None,
             kex_algs=kex_algs, client_version=client_version)
 
         try:
@@ -174,28 +179,34 @@ async def _get_server_host_key(target, *, tunnel=(), family=(), flags=0,
                                                           loop=current_loop,
                                                           flags=flags,
                                                           conn_factory=conn_factory,
-                                                          msg='Fetching server host key from',
+                                                          msg="Fetching server host key from",
                                                           sock=None)
             except OSError as oserror:
                 if oserror.errno == 113:
+                    error = f"dial tcp {host}:{port}: connect: no route to host"
                     break
             else:
                 server_host_key = conn.get_server_host_key()
                 result[server_host_key_algs] = server_host_key
                 if not result.get("version"):
                     try:
-                        server_version = conn.get_extra_info('server_version')
+                        server_version = conn.get_extra_info("server_version")
                         result["version"] = server_version
                     except:
                         result["version"] = ""
                 conn.abort()
                 await conn.wait_closed()
         except:
-            pass
-    return result if result else None
+            error = "unknow-error"
+    if not result and not error:
+        error = "unknow-error"
+    return result, error
 
 
-async def worker_single_fingerprint(target, semaphore: asyncio.Semaphore, queue_out: asyncio.Queue) -> None:
+async def worker_single_fingerprint(target,
+                                    semaphore: asyncio.Semaphore,
+                                    queue_out: asyncio.Queue,
+                                    timeout: int = CONST_TIMEOUT) -> None:
 
     value = {}
     error = ''
@@ -203,29 +214,34 @@ async def worker_single_fingerprint(target, semaphore: asyncio.Semaphore, queue_
 
     async with semaphore:
         try:
-            future_connection = _get_server_host_key(target=target, client_version='AsyncSSHChecker')
-            if result := await asyncio.wait_for(future_connection, timeout=10):
+            result, error_message = await asyncio.wait_for(
+                    _get_server_host_key(target=target, client_version="AsyncSSH-Fingerprint"),
+                    timeout=timeout)
+            if result:
                 value["version"] = result.get("version")
                 for name, algorithms in CONST_SERVER_HOST_KEY_ALGS.items():
                     if data := result.get(name):
                         value[name] = {}
                         for algorithm in algorithms:
-                            function_hash_name = algorithm.__name__.replace('openssl_', '')
+                            function_hash_name = algorithm.__name__.replace("openssl_", "")
                             value[name][function_hash_name] = algorithm(data.public_data).hexdigest()
-                status = True
+                    status = True
+            else:
+                error = error_message
         except asyncio.TimeoutError:
             error = "timeout-error"
         except:
             error = "unknown-error"
     if error or not status:
         value["error"] = "unknown-error" if not error else error
-    await queue_out.put((value, status))
+    await queue_out.put((value, target, status))
 
 
 async def work_with_create_tasks_queue(queue_with_input: asyncio.Queue,
                                        queue_with_tasks: asyncio.Queue,
                                        queue_out: asyncio.Queue,
-                                       semaphore: asyncio.Semaphore) -> None:
+                                       semaphore: asyncio.Semaphore,
+                                       timeout: int) -> None:
 
     while True:
         item = await queue_with_input.get()  # item is a Target
@@ -233,8 +249,9 @@ async def work_with_create_tasks_queue(queue_with_input: asyncio.Queue,
             await queue_with_tasks.put(CONST_STOP_STAGE)
             break
         elif item:
-            task = asyncio.create_task(worker_single_fingerprint(item, semaphore, queue_out))
-            await queue_with_tasks.put(task)
+            await queue_with_tasks.put(
+                asyncio.create_task(worker_single_fingerprint(item, semaphore, queue_out, timeout=timeout))
+            )
 
 
 async def work_with_queue_result(queue_out: asyncio.Queue,
@@ -247,18 +264,46 @@ async def work_with_queue_result(queue_out: asyncio.Queue,
         method_write_result = write_to_file
     else:
         method_write_result = write_to_stdout
+    _z = datetime.datetime.now(datetime.timezone(datetime.timedelta(0))).astimezone().tzinfo
     async with aiofiles_open(filename, mode=mode_write) as file_with_results:
         while True:
             value: tuple[dict, bool] | bytes = await queue_out.get()
             if value == CONST_STOP_STAGE:
                 break
-            line, status = value
+            line, target, status = value
             if status:
                 number_of_successes += 1
+
+                # region copy like from zgrab2 ssh module
+                result_row = {"ip": target.ip,
+                              "port": target.port,
+                              "data":{"ssh": {}}}
+                result_row["data"]["ssh"]["status"] = "success"
+                result_row["data"]["ssh"]["protocol"] = "ssh"
+                result_row["data"]["ssh"]["timestamp"] = datetime.datetime.now(tz=_z).strftime("%Y-%m-%dT%H:%M:%S%z")
+                result_row["data"]["ssh"]["result"] = {}
+                result_row["data"]["ssh"]["result"]["server_id"] = {}
+                result_row["data"]["ssh"]["result"]["server_id"]["raw"] = line.get("version", "")
+                result_row["data"]["ssh"]["result"]["key_exchange"] = {}
+                result_row["data"]["ssh"]["result"]["key_exchange"] = {}
+                result_row["data"]["ssh"]["result"]["key_exchange"]["server_host_key"] = {}
+                if "version" in line:
+                    line.pop("version")
+                result_row["data"]["ssh"]["result"]["key_exchange"]["server_host_key"]["algorithms"] = line
+                # endregion
             else:
+                # region copy like from zgrab2 ssh module
                 number_of_failed += 1
-            record = ujson_dumps(line)
-            await method_write_result(file_with_results, record)
+                result_row = {"ip": target.ip,
+                              "port": target.port,
+                              "data": {"ssh": {}}}
+                result_row["data"]["ssh"]["status"] = "error"
+                result_row["data"]["ssh"]["protocol"] = "ssh"
+                result_row["data"]["ssh"]["result"] = {}
+                result_row["data"]["ssh"]["timestamp"] = datetime.datetime.now(tz=_z).strftime("%Y-%m-%dT%H:%M:%S%z")
+                result_row["data"]["ssh"]["error"] = line["error"]
+                # endregion
+            await method_write_result(file_with_results, ujson_dumps(result_row))
 
     duration_time_sec = round(time() - start_time, 4)
     return duration_time_sec, number_of_successes, number_of_failed
@@ -276,7 +321,7 @@ async def main(settings):
     mode_write = settings["mode_write"]
 
     read_input = method_create_targets(queue_input, settings, path_to_file_targets)  # create targets
-    create_tasks = work_with_create_tasks_queue(queue_input, queue_results, queue_prints, task_semaphore)  # execution
+    create_tasks = work_with_create_tasks_queue(queue_input, queue_results, queue_prints, task_semaphore, settings["timeout"])  # execution
     execute_tasks = work_with_queue_tasks(queue_results, queue_prints)
     print_output = work_with_queue_result(queue_prints, output_file, mode_write)
 
@@ -290,12 +335,12 @@ async def main(settings):
         # {"statuses":{"ssh":{"successes":4,"failures":1}},"start":"2023-10-09T00:42:17+03:00","end":"2023-10-09T00:42:20+03:00","duration":"3.067355464s"}
         count_input = task_read_in.result()
         duration_time_sec, count_good, count_error = task_output.result()
-        statistics_output = {"statuses":{"ssh":{"successes": count_good,
-                                                "failures": count_error,
-                                                "valid": count_input}},
+        statistics_output = {"statuses": {"ssh": {"successes": count_good,
+                                                  "failures": count_error,
+                                                  "valid": count_input}},
                              "duration": duration_time_sec}
-        async with aiofiles_open('/dev/stdout', mode='wb') as stats:
-            await stats.write(ujson_dumps(statistics_output).encode('utf-8') + b'\n')
+        async with aiofiles_open("/dev/stdout", mode="wb") as stats:
+            await stats.write(ujson_dumps(statistics_output).encode("utf-8") + b"\n")
 
 
 def create_settings(args) -> dict | None:
@@ -327,6 +372,7 @@ def create_settings(args) -> dict | None:
     settings["mode_write"] = mode_write
 
     settings["statistics"] = args.statistics
+    settings["timeout"] = args.timeout
     return settings
 
 
@@ -335,14 +381,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "-f",
         "--input-file=",
-        dest='input_file',
+        dest="input_file",
         type=str,
         help="path to file with targets")
     
     parser.add_argument(
         "-o",
         "--output-file=",
-        dest='output_file',
+        dest="output_file",
         type=str,
         help="path to file with results")
     
@@ -362,11 +408,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s",
         "--senders=",
-        dest='senders',
+        dest="senders",
         type=int,
         default=CONST_C,
         help=f"Number of send coroutines to use (default: {CONST_C})")
-    
+
+    parser.add_argument(
+        "-t",
+        "--timeout=",
+        dest='timeout',
+        type=int,
+        default=CONST_TIMEOUT,
+        help=f"Timeout for connect to SSH (default: {CONST_TIMEOUT})")
+
     parser.add_argument(
         "--show-statistics",
         dest="statistics",
